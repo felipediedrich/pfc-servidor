89565db9c88ce976b42a690e9965eee1046136ce
Felipe Diedrich <felipe.dmda@gmail.com>
2021-05-18 21:33:10 -0300

    undo

--- iot/admin.py
+++ iot/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from iot.models import Dispositivo, Agendamento
+
+class DispositivoAdmin(admin.ModelAdmin):
+    list_display = ('name','status','user')
+    list_filter = ('user',)
+
+class AgendamentoAdmin(admin.ModelAdmin):
+    list_display = ('name','horario')
+
+admin.site.register(Dispositivo,DispositivoAdmin)
+admin.site.register(Agendamento,AgendamentoAdmin)
--- iot/models.py
+++ iot/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Dispositivo(models.Model):
+    id = models.AutoField(primary_key=True)
+    hardware_id = models.CharField(max_length=255)
+    name = models.CharField(null=False,max_length=100)
+    status = models.BooleanField(default=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+
+class Agendamento(models.Model):
+    id = models.AutoField(primary_key=True)
+    dispositivo = models.ForeignKey('Dispositivo', on_delete=models.CASCADE,related_name='datacollector')
+    name = models.CharField(null=False,max_length=100)
+    horario = models.TimeField()
+
+# class Dias(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     dia = models.CharField(max_length=1,choices=((0,'Domingo'),(1,'Segunda'),(2,'Terça'),(3,'Quarta'),(4,'Quinta'),(5,'Sexta'),(6,'Sabado')))
