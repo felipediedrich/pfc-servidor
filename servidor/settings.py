f3b4f21f9b64a08f6346bb5c6d76347095c6b5af
Felipe Diedrich <felipe.dmda@gmail.com>
2021-07-15 20:06:28 -0300

    Adjust

--- servidor/settings.py
+++ servidor/settings.py
@@ -55,9 +55,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+MIDDLEWARE_CLASSES = (
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-]
+)
 
 ROOT_URLCONF = 'servidor.urls'
 
