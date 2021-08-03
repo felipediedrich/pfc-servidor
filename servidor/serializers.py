0e268b9afb1a4b10471ae53b53bfe1f1626c924a
Felipe Diedrich <felipe.dmda@gmail.com>
2021-08-03 19:23:35 -0300

    User serializer

--- servidor/serializers.py
+++ servidor/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        help_text='Leave empty if no change needed',
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
+
     class Meta:
         model = User
-        fields = ['url', 'username']
+        fields = ('url', 'username', 'email', 'password')
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super(UserSerializer, self).create(validated_data)
