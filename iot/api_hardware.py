c3ec2a4cf0168a83f2bf45a72e881c6ed06fc251
Felipe Diedrich <felipe.dmda@gmail.com>
2021-08-03 22:46:01 -0300

    Update API hardware

--- iot/api_hardware.py
+++ iot/api_hardware.py
@@ -13,17 +13,18 @@ class OnOff(View):
     def post(self, request, mac):
 
         dispositivo = Dispositivo.objects.filter(mac=mac).first()
+        parameters = json.loads(request.body)
 
         if dispositivo == None: return JsonResponse({"message":"MAC Address don't exist!"}, status=400)
-        elif 'status' not in request.POST: return JsonResponse({"message":"Status don't sended!"}, status=400)
-        elif request.POST['status'] not in ['0','1']: return JsonResponse({"message":"Invalid, send status equals '1' or '0'"}, status=400)
+        elif 'status' not in parameters: return JsonResponse({"message":"Parameter 'status' don't sended!"}, status=400)
+        elif parameters['status'] not in [0,1]: return JsonResponse({"message":"Invalid, send status equals 1 or 0"}, status=400)
         
-        elif request.POST['status'] == '1':
+        elif parameters['status'] == 1:
             dispositivo.status = True
             dispositivo.save()
             return JsonResponse({'message':'ok','status':1}, status=200)
 
-        elif request.POST['status'] == '0':
+        elif parameters['status'] == 0:
             dispositivo.status = False
             dispositivo.save()
             return JsonResponse({'message':'ok','status':0}, status=200)
