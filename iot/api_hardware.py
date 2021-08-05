0d21eddb36bb4686a6985285eea351bec1f716c7
Felipe Diedrich <felipe.dmda@gmail.com>
2021-08-05 01:57:27 -0300

    Uppdate agendamento

--- iot/api_hardware.py
+++ iot/api_hardware.py
@@ -1,10 +1,11 @@
 from django.views import View
 from django.http import JsonResponse
 import json
-from .models import Dispositivo
+from .models import Dispositivo, Agendamento
 from django.http import HttpResponseBadRequest
-from datetime import datetime
-
+import pytz
+from datetime import datetime, timedelta
+from django.db.models import Q
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
@@ -18,14 +19,15 @@ class OnOff(View):
         if dispositivo == None: return JsonResponse({"message":"MAC Address don't exist!"}, status=400)
         elif 'status' not in parameters: return JsonResponse({"message":"Parameter 'status' don't sended!"}, status=400)
         elif parameters['status'] not in [0,1]: return JsonResponse({"message":"Invalid, send status equals 1 or 0"}, status=400)
-        
         elif parameters['status'] == 1:
             dispositivo.status = True
+            dispositivo.last_ping = datetime.now()
             dispositivo.save()
             return JsonResponse({'message':'ok','status':1}, status=200)
 
         elif parameters['status'] == 0:
             dispositivo.status = False
+            dispositivo.last_ping = datetime.now()
             dispositivo.save()
             return JsonResponse({'message':'ok','status':0}, status=200)
 
@@ -33,10 +35,21 @@ class OnOff(View):
         dispositivo = Dispositivo.objects.filter(mac=mac).first()
 
         if dispositivo == None: return JsonResponse({"message":"MAC Address don't exist!"}, status=400)
-        elif not dispositivo.status: msg = JsonResponse({"status": 0 })
-        else: msg = JsonResponse({"status": 1 })
+
+        # Parameters
+        last_ping = dispositivo.last_ping.astimezone(pytz.timezone('America/Sao_Paulo')).time()
+        now = datetime.now().time()
+        weekday = str((datetime.now() + timedelta(days=1)).weekday())
+
+        agendamento = Agendamento.objects.filter(dispositivo__mac = mac,horario__gte = last_ping,horario__lte = now,repetir__contains = weekday).first()
+
+        if agendamento != None:
+            dispositivo.status = agendamento.modo
+            if "T" in agendamento.repetir: agendamento.delete()
 
         dispositivo.last_ping = datetime.now()
         dispositivo.save()
 
-        return msg
+        if not dispositivo.status: return JsonResponse({"status": 0 })
+        else: return JsonResponse({"status": 1 })
+
