4d6c2e24b41ce11cd541d548b677b337e942237b
Felipe Diedrich <felipe.dmda@gmail.com>
2021-10-05 23:35:04 -0300

    List is_110

--- iot/serializers.py
+++ iot/serializers.py
@@ -5,7 +5,7 @@ from iot.models import Dispositivo, Agendamento
 class DispositivoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dispositivo
-        fields = ['url','mac','name','icon','status','last_ping']
+        fields = ['url','mac','name','is_110','icon','status','last_ping']
 
 class AgendamentoSerializer(serializers.ModelSerializer):
     class Meta:
