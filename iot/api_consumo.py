b831de2c3c2a878b44e8cf8531b961e5a4f8579b
Felipe Diedrich <felipe.dmda@gmail.com>
2021-10-14 19:38:34 -0300

    Consumo Adjust

--- iot/api_consumo.py
+++ iot/api_consumo.py
@@ -1,7 +1,7 @@
 from django.views import View
 from django.http import JsonResponse
 # from pandas.core.resample import resample
-from .models import Consumo
+from .models import Consumo, Dispositivo
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 import pandas as pd
@@ -14,6 +14,13 @@ class SeriesConsumo(View):
         if freq.lower() not in ['h','d','w','m']: return JsonResponse({"message":"Frequency not in ('h','w','d','m')"}, status=400)
     
         dados = Consumo.objects.filter(dispositivo__mac = mac).values('horario','corrente')
-        resample = pd.DataFrame(dados).resample(freq, on='horario').corrente.mean().round(2).fillna('NULL')
+        is_110 = Dispositivo.objects.filter(mac = mac).values('is_110').first()['is_110']
+
+        resample = pd.DataFrame(dados).resample(freq, on='horario').corrente.mean()
+
+        if is_110: resample *= 127
+        else: resample*=220
+
+        resample = resample.round(2).fillna('NULL')
 
         return JsonResponse({'x':resample.index.tolist(),'y':resample.tolist()})
